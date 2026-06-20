c241a552d68a8ad46d82300a2b939c91f4a86681
diogoalbq <diogo17092007@gmail.com>
2026-06-20 16:21:05 -0300

    Update via Manager v5.0 — 2026-06-20 16:21

--- src/data.tsx
+++ src/data.tsx
@@ -220,7 +220,9 @@ export interface PortfolioItem {
 }
 
 export const artItems: PortfolioItem[] = [
-  ];
+  
+  { type: 'image', color: 'from-amber-200 to-yellow-200', iconColor: 'text-amber-600', icon: <ImageIcon className="w-10 h-10" />, mediaUrl: 'https://res.cloudinary.com/dnbdnjpd4/image/upload/v1781967545/Tame_gkh52r.png' },
+];
 
 export const videoItems: PortfolioItem[] = [
   ];
